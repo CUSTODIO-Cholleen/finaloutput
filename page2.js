3dd97e3916a7b85b7a03c8beae5ff782ea12361c
CUSTODIO-Cholleen <custodiocholleen0@gmail.com>
2026-03-06 22:13:57 +0800

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,6 @@
+alert("Welcome to the Movies page!");
+
+var movie = prompt("type your favorite movie genre and go explore our collection!");
+alert("Nice taste!");
+
+alert("Enjoy browsing our movie collection!");
---
+++ page3.js
@@ -0,0 +1,6 @@
+alert("Welcome to the Contact Page!");
+
+var email = prompt("Enter your email if you want updates:");
+alert("Thank you! We will contact you at: " + email);
+
+alert("Thank you for visiting BlueBox!");
